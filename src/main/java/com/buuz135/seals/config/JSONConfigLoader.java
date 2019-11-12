88b91eec7d4c7e341f6480b1275b4a9d6dc913a7
Buuz135 <buuz135@hotmail.com>
2019-11-12 18:08:10 +0100

    Fixed secret advancements icons

--- src/main/java/com/buuz135/seals/config/JSONConfigLoader.java
+++ src/main/java/com/buuz135/seals/config/JSONConfigLoader.java
@@ -5,14 +5,18 @@ import com.buuz135.seals.Seals;
 import com.buuz135.seals.client.icon.IIcon;
 import com.buuz135.seals.client.icon.ItemStackIcon;
 import com.google.gson.*;
+import net.minecraft.block.Blocks;
 import net.minecraft.client.entity.player.ClientPlayerEntity;
 import net.minecraft.entity.player.ServerPlayerEntity;
+import net.minecraft.item.Items;
 import net.minecraft.util.ResourceLocation;
 
-import java.io.*;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.lang.reflect.Type;
 import java.util.Arrays;
-import java.util.UUID;
 
 public class JSONConfigLoader {
 
@@ -62,8 +66,9 @@ public class JSONConfigLoader {
                 return entity.getUniqueID().toString().equals("d28b7061-fb92-4064-90fb-7e02b95a72a6");
             }
         };
-        info.setRequisites(new ResourceLocation[]{new ResourceLocation("minecraft:story/root")});
+        //info.setRequisites(new ResourceLocation[]{new ResourceLocation("minecraft:story/root")});
         info.setInvisible();
+        info.setIcon(new ItemStackIcon(Blocks.FURNACE.getRegistryName()));
         Seals.SEAL_MANAGER.getSeals().add(info);
         info = new SealInfo(new ResourceLocation("seals:patreon"), "munificent"){
             @Override
@@ -76,8 +81,9 @@ public class JSONConfigLoader {
                 return Seals.PATREONS.stream().anyMatch(uuid -> uuid.equals(entity.getUniqueID()));
             }
         };
-        info.setRequisites(new ResourceLocation[]{new ResourceLocation("minecraft:story/root")});
+        //info.setRequisites(new ResourceLocation[]{new ResourceLocation("minecraft:story/root")});
         info.setInvisible();
+        info.setIcon(new ItemStackIcon(Items.NETHER_STAR.getRegistryName()));
         Seals.SEAL_MANAGER.getSeals().add(info);
     }
 
